96ea3e62daf42182dbe00058c5c63ae7cfdb5b3e
MatiasForconi <matiasfernandoforconi@hotmail.com>
2020-11-21 19:19:36 -0300

    Adding background image and js

---
+++ js/game.js
@@ -0,0 +1,14 @@
+var boardHTML = null,
+    columnsHTML = null,
+    winHTML = null,
+    winDescHTML = null,
+    boardArray = [
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        ];
+        
