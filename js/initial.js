fb850c67d0f07f40e2e8dc22a65cdb0abfb387a2
MatiasForconi <matiasfernandoforconi@hotmail.com>
2020-11-22 15:09:54 -0300

    Adding initial.js and modifying the style

---
+++ js/initial.js
@@ -0,0 +1,28 @@
+var timeP1HTML = null,
+    timeP2HTML = null,
+    timeMP1HTML = null,
+    timeSP1HTML = null,
+    timeMP2HTML = null,
+    turn = 'red',
+    timeSP2HTML = null;
+var init = function () {
+    turn = Math.random() > 0.5 ? 'red' : 'blue';
+    turn = turn = Math.random() > 0.5 ? 'red' : 'blue';
+    boardHTML = document.getElementById('board');
+    timeP1HTML = document.getElementById('timeP1');
+    timeP2HTML = document.getElementById('timeP2');
+    timeMP1HTML = document.getElementById('timeMP1');
+    timeSP1HTML = document.getElementById('timeSP1');
+    timeMP2HTML = document.getElementById('timeMP2');
+    timeSP2HTML = document.getElementById('timeSP2');
+    Players2 = document.getElementById('Players2');
+    contact = document.getElementById('contact');
+    home = document.getElementById('home');
+    back = document.getElementById('back');
+    Players2.onclick = goGame;
+    contact.onclick = goContact;
+    home.onclick = goHome;
+    back.onclick = goHome;
+    goHome();
+}
+window.onload = init;
