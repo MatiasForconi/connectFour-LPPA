b7474e8d50a3bdefa5b0af916a04f5dbe99d3a8b
MatiasForconi <matiasfernandoforconi@hotmail.com>
2020-11-23 01:12:55 -0300

    Adding funcionality to board

--- js/controller.js
+++ js/controller.js
@@ -9,11 +9,11 @@ var goHome = function(){
   var goContact = function(){
     document.getElementById('contactScreen').style.display= 'flex';
     document.getElementById('initialScreen').style.display= 'none';
-    render();
   }
 
-  var goWin = function(){
+  var goGame = function(){
     document.getElementsByClassName('main')[0].style.display = 'none';
     document.getElementsByClassName('footer')[0].style.display = 'none';
     document.getElementById('winScreen').style.display= 'flex';
+    render();
   }
